180869d6ceed7614978d05aaf7bd85014a1780db
Cameron <cmclaren@tulane.edu>
2023-11-10 23:38:07 -0600

    Code & questions

--- answers.md
+++ answers.md
@@ -1,8 +1,19 @@
 # CMPS 2200 Assignment 4
 ## Answers
 
-**Name:**_________________________
+**Name:** Cameron McLaren
 
 
 Place all written answers from `assignment-04.md` here for easier grading.
 
+1a) To produce as few coins as possible given N dollars, a largest denomination first greedy algorithm would work. Essentially, we take as many coins with the largest value possible. Once there's not enough left to keep using that coin denomination, we decrease to the next highest coin denomination. Then we keep doing this process until the exact amount is exchanged.
+
+1b) Given optimal sequence of coins C, where C(i) is the number of coins with the denomination 2^i. Therefore, for all i < k, C(i) < 2. Then, for 2^k we just select as many coins as possible. This process represents the greedy algorithm described above, making it most optimal.
+
+1c) The work and span of the algorithm is O(log(N)) since for each time we go to a smaller coin value, we reduce N faster than we reduce the number of coins of each denomination.
+
+2a) Given we have coin
+
+2b) C(N, i) is the minimum number of coins needed to make change for n.
+
+2c) The work and span for the memoized approach is O(N*k) because the number of distinct subproblems is N*k
--- main.py
+++ main.py
@@ -16,16 +16,66 @@ def MED(S, T):
         if (S[0] == T[0]):
             return(MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return(1 + min(MED(S, T[1:]), MED(S[1:], T[1:])))
 
 
 def fast_MED(S, T, MED={}):
     # TODO -  implement top-down memoization
+    length_S = len(S)
+    length_T = len(T)
+    
+    med = []
+    for i in range(length_S + 1):
+        row = []
+        for j in range(length_T + 1):
+            row.append(0)
+        med.append(row)
+    
+    for i in range(length_S + 1):
+        for j in range(length_T + 1):
+            if i == 0:
+                med[i][j] = j
+            elif j == 0:
+                med[i][j] = i
+            elif S[i - 1] == T[j - 1]:
+                med[i][j] = med[i - 1][j - 1]
+            else:
+                med[i][j] = 1 + min(med[i][j - 1])
     pass
 
 def fast_align_MED(S, T, MED={}):
     # TODO - keep track of alignment
-    pass
+    fMED = fast_MED(S, T)
+    new_S = []
+    new_T = []
+    i = len(S)
+    j = len(T)
+    while True:
+      if(i == 0 and j==0):
+        break
+      else:
+        insert = fMED[i][j-1]
+        remove = fMED[i-1][j]
+        sub = fMED[i-1][j-1]
+        minimum = min(insert,remove,sub)
+        if(sub == minimum):
+          new_S = [S[i-1]] + new_S
+          new_T = [T[j-1]] + new_T
+          i = i-1                        
+          j = j-1
+        elif(insert == minimum):
+          new_S = ['-'] + new_S
+          new_T = [T[j-1]] + new_T
+          j = j-1
+        elif(remove == minimum):
+          new_T = ['-'] + new_T
+          new_S = [S[i-1]] + new_S
+          i = i-1
+    
+    s_str = "".join(new_S) 
+    t_str = "".join(new_T)
+    
+    return s_str, t_str
 
 def test_MED():
     for S, T in test_cases:
